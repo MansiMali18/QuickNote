f5210eb7a78ecde9507d8229b91d6a3df8148854
MansiMali18 <mansimali399@gmail.com>
2024-09-10 12:11:54 +0530

    basic code added for all pages

--- src/index.js
+++ src/index.js
@@ -1,9 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-
+import { createBrowserRouter,RouterProvider } from 'react-router-dom';
+import Home from "./../src/views/HOME/Home"
+import Add from "./../src/views/ADD/Add"
+import Show from "./../src/views/SHOW/Show"
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const router=createBrowserRouter([
+  {
+    path: "/",
+    element: <Home/>
+  },
+  {
+    path: "/Add",
+    element: <Add/>
+  }, {
+    path: "/Show",
+    element: <Show/>
+  }
+])
 root.render(
- <h1>Quick note</h1>
+<RouterProvider router={router}/>
 );
 
---
+++ src/views/SHOW/Show.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import './Show.css'
+function Show() {
+  return (
+    <div>Show</div>
+  )
+}
+
+export default Show
